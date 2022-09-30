9239dba1a37d348b3c247e0bc9e69697e41a7704
MuddathirDawood <dmuddathir@gmail.com>
2022-09-30 11:31:30 +0200

    camera, canvas and file uploading-part3

--- src/pages/CameraPage.vue
+++ src/pages/CameraPage.vue
@@ -4,8 +4,13 @@
       <video class="full-width" autoplay playsinline ref="video" v-show="!imageCaptured"/>
       <canvas ref="canvas" class="full-width" height="240" v-show="imageCaptured"/>
     </div>
-    <div class="text-center">
-      <q-btn round color="grey-10" icon="camera" @click="captureImg"/>
+    <div class="text-center q-pa-md">
+      <q-btn round color="grey-10" icon="camera" @click="captureImg" v-if="enabledCamera"/>
+      <q-file outlined v-model="imageUpload" v-else label="Choose An Image" accept="image/*" @input="capImgFallback">
+        <template v-slot:prepend>
+          <q-icon name="eva-attach-outline" />
+        </template>
+      </q-file>
     </div>
     <div class="row justify-center q-ma-md">
       <q-input v-model="post.caption" class="col col-sm-8" label="Caption" dense/>
@@ -13,7 +18,7 @@
     <div class="row justify-center q-ma-md">
       <q-input v-model="post.location" class="col col-sm-8" label="Location" dense>
         <template v-slot:append>
-          <q-btn round dense flat icon="eva-navigation-2-outline" />
+          <q-btn round dense flat icon="eva-navigation-2-outline" @click="getLocation"/>
         </template>
       </q-input>
     </div>
@@ -39,7 +44,9 @@ export default defineComponent({
         image: null,
         date: Date.now()
       },
-      imageCaptured: false
+      imageCaptured: false,
+      imageUpload: [],
+      enabledCamera: true
     }
   },
   methods:{
@@ -48,6 +55,8 @@ export default defineComponent({
         video: true
       }).then(stream => {
         this.$refs.video.srcObject = stream
+      }).catch(err =>{
+        this.enabledCamera = false
       })
     },
     captureImg(){
@@ -58,7 +67,44 @@ export default defineComponent({
       let context = canvas.getContext('2d')
       context.drawImage(video, 0, 0, canvas.width, canvas.height)
       this.imageCaptured = true
-    }
+      this.post.image = this.dataURItoBlob(canvas.toDataURL())
+      this.disableCamera()
+    },
+    dataURItoBlob(dataURI) {
+      var byteString = atob(dataURI.split(',')[1]);
+      var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+      var ab = new ArrayBuffer(byteString.length);
+      var ia = new Uint8Array(ab);
+      for (var i = 0; i < byteString.length; i++) {
+          ia[i] = byteString.charCodeAt(i);
+      }
+      var blob = new Blob([ab], {type: mimeString});
+      return blob;
+    },
+    capImgFallback(file){
+      this.post.image = file.target.files[0]
+      let canvas = this.$refs.canvas
+      let context = canvas.getContext('2d')
+
+      var reader = new FileReader()
+      reader.onload = event => {
+          var img = new Image()
+          img.onload = () => {
+            canvas.width = img.width
+            canvas.height = img.height
+            context.drawImage(img,0,0)
+            this.imageCaptured = true
+          }
+          img.src = event.target.result
+      }
+      reader.readAsDataURL(file.target.files[0])
+    },
+    disableCamera(){
+      this.$refs.video.srcObject.getVideoTracks().forEach(track => {
+        track.stop()
+      })
+    },
+
   },
   mounted(){
     this.initCamera()
